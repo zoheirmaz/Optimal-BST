9fe0138798603357ef2743380687cac8f26b9e9b
zmf <zoheiramz.zm@gmail.com>
2018-12-10 00:46:09 +0330

    init commi

---
+++ functions.py
@@ -0,0 +1,24 @@
+from binarytree import Node
+
+
+def tree(keys, k, i, j):
+    r = k[i][j]
+
+    if i == j:
+        root = Node(keys[r])
+
+    else:
+        root = Node(keys[r])
+        if i <= r - 1:
+            root.left = tree(keys, k, i, r - 1)
+        if r + 1 <= j:
+            root.right = tree(keys, k, r + 1, j)
+
+    return root
+
+
+def summ(P, I, J):
+    sum_p = 0
+    for m in range(I, J + 1):
+        sum_p += P[m]
+    return sum_p
---
+++ main.py
@@ -0,0 +1,53 @@
+from functions import tree, summ
+
+n = int(input('Enter n: '))
+
+keys = []
+freq = []
+
+for i in range(n):
+    t = int(input('Enter key: '))
+    keys.append(t)
+
+    t = int(input('Enter freq: '))
+    freq.append(t)
+
+R = [[0 for x in range(n + 1)] for y in range(n + 1)]
+
+A = [[0 for x in range(n + 1)] for y in range(n + 1)]
+
+for i in range(n):
+    A[i][i - 1] = 0
+    A[i][i] = freq[i]
+    R[i][i] = i
+    R[i][i - 1] = 0
+
+# ------------------------------------------
+
+for diagonal in range(n):
+
+    for i in range(n - diagonal):
+
+        j = i + diagonal
+
+        if i < j:
+
+            min_list = []
+            for k in range(i, j + 1):
+                min_list.append(((A[i][k - 1] + A[k + 1][j]), k))
+
+            # pprint(min_list)
+            min_list.sort()
+
+            A[i][j] = min_list[0][0] + summ(freq, i, j)
+
+            # R[i][j] = a value of k that gave the minimum;
+            R[i][j] = min_list[0][1]
+
+# pprint(A)
+# print()
+# pprint(R)
+
+print(tree(keys, R, 0, n - 1))
+print()
+print('Cost of Optimal binary search is ' + str(A[0][n - 1]))
